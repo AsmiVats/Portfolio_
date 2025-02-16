b675d62be9ea997014dbdbf021e4e9b28ead0dec
Asmi Vats <asmivats15@gmail.com>
2025-02-16 23:38:10 +0530

    route in navbar

--- app/pages/Navbar.tsx
+++ app/pages/Navbar.tsx
@@ -43,10 +43,10 @@ function Navbar() {
       <div
         className={`fixed top-0 right-0 h-full w-64 bg-[#4c6ee2] transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out md:hidden flex flex-col items-center pt-20 space-y-6 shadow-lg z-[1050]`}
       >
-        <div className="text-white text-[20px] font-semibold font-degular" onClick={() => setIsOpen(false)}>Portfolio</div>
-        <div className="text-white text-[20px] font-semibold font-degular" onClick={() => setIsOpen(false)}>Services</div>
-        <div className="text-[#d3f86e] text-[20px] font-semibold font-degular" onClick={() => setIsOpen(false)}>About</div>
-        <a className="relative mt-4" href="#">
+        <a href='#home' className="text-white text-[20px] font-semibold font-degular" onClick={() => setIsOpen(false)}>Portfolio</a>
+        <a href='#services' className="text-white text-[20px] font-semibold font-degular" onClick={() => setIsOpen(false)}>Services</a>
+        <a href='#about' className="text-[#d3f86e] text-[20px] font-semibold font-degular" onClick={() => setIsOpen(false)}>About</a>
+        <a className="relative mt-4" href="mailto:asmivats15@gmail.com">
           <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded-full bg-black"></span>
           <span className="flex items-center gap-1 relative rounded-full border-2 border-black bg-[#d3f86e] px-4 py-2 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
             Contact me
